dca477daddbbda617f36666d25b10f94fd1335d6
Thiago Rocha Moreira Paiva <pste13.informatica@crecisp.gov.br>
2021-06-19 13:37:55 -0300

    Primerao

---
+++ app/Http/Controllers/Web/PageController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Web;
+
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function dashboard(){
+        return Inertia::render('Dashboard');
+    }
+    public function welcome(){
+        return Inertia::render('Welcome', [
+            'canLogin' => Route::has('login'),
+            'canRegister' => Route::has('register'),
+            'laravelVersion' => Application::VERSION,
+            'phpVersion' => PHP_VERSION,
+        ]);
+    }
+}
